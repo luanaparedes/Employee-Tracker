240290074096ac4ae8e37d72ec87c17cfd88380a
luanaparedes <lua123na@gmail.com>
2023-01-13 10:41:25 -0500

    added db and modules to server js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mysql = require('mysql2');
+// const PORT?
+const app = express();
+const inquirer = require('inquirer');
+
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
